87b72e994e9fcd4960b15d15491df50c87284227
Avizal <avizal@protonmail.com>
2021-09-19 23:15:55 +0300

    feat: added interaction with settings

---
+++ app/classes/Settings.php
@@ -0,0 +1,47 @@
+<?php
+
+class Settings
+{
+    protected $fileSettings = "settings.json";
+    protected $settings = [];
+
+    public function __construct()
+    {
+        $this->getSettings();
+    }
+
+    protected function getSettings()
+    {
+        $filePath = PATH_CONFIG_DIR . "/" . $this->fileSettings;
+
+        if (file_exists($filePath)) {
+            $data = file_get_contents($filePath);
+            $data = json_decode($data, true);
+
+            foreach ($data as $param => $item) {
+                if (is_string($item)) {
+                    $this->setSetting($param, $item);
+                } elseif (is_array($item)) {
+                    // Todo: Добавить взаимодействие с массивами
+                    $this->setSetting($param, $item);
+                }
+            }
+        }
+
+    }
+
+    public function setSetting(string $key, $value)
+    {
+        $this->settings[$key] = $value;
+    }
+
+    public function getSetting(string $key)
+    {
+        return $this->settings[$key] ?? false;
+    }
+
+    public function getArraySettings(): array
+    {
+        return $this->settings;
+    }
+}
--- config/config.php
+++ config/config.php
@@ -2,3 +2,4 @@
 
 const PATH_CLASSES = PATH_DIR . "/app/classes";
 const PATH_STARTUP = PATH_CLASSES . "/Startup.php";
+const PATH_CONFIG_DIR = PATH_DIR . "/config";
